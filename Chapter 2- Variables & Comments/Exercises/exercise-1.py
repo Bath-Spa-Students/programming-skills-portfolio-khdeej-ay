d552f77b1d31cb94f7e9e1b1c1720ea26424e977
Khadeeja <s.khdeej@gmail.com>
2023-09-29 21:59:45 +0400

    feat: add chp2 ex-1 & ex-2

---
+++ Chapter 2- Variables & Comments/Exercises/exercise-1.py
@@ -0,0 +1,8 @@
+# Assign a message to a variable, and print that message.
+# Then change the value of the variable to a new message, and print the new message.
+
+message = "This is the first exercise"
+print(message)
+
+message = "This is the first exercise in the second chapter"
+print(message)
---
+++ Chapter 2- Variables & Comments/Exercises/exercise-2.py
@@ -0,0 +1,3 @@
+# Find a quote from a famous person you admire. Print the quote and the name of its author. 
+
+print("Sappho once said, \"When anger spreads through the breath, guard thy tongue from barking idly\". ")

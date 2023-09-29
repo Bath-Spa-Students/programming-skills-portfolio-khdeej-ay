45e9ab35bc885849cd4dd4da28138bb6a45f4b88
Khadeeja <s.khdeej@gmail.com>
2023-09-29 22:09:08 +0400

    feat: add chp2 ex-5

---
+++ Chapter 2- Variables & Comments/Exercises/exercise-5.py
@@ -0,0 +1,9 @@
+# A girl wants to buy USBs for £50. They are £6 each.
+# Write a programme that calculates how many USBs she can buy and how many pounds she will have left.
+
+total_cash = 50
+one_usb = 6
+max_usb = 50 // 6
+left_cash = 50 % 6
+print("Total USB sticks she can buy:", max_usb)
+print("Pounds she has left:", left_cash)

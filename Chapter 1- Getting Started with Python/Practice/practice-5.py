f269b8064d32ea9cf74deba465820032998edb62
Khadeeja <s.khdeej@gmail.com>
2023-10-02 12:05:59 +0400

    feat: add chp1 pract-5

---
+++ Chapter 1- Getting Started with Python/Practice/practice-5.py
@@ -0,0 +1,8 @@
+'''Write four strings in different variables and 
+print the output as one string (String Concatenation)'''
+
+sad1 = "Nice "
+sad2 = "to "
+sad3 = "meet "
+sad4 = "you." 
+print(sad1 + sad2 + sad3 + sad4)

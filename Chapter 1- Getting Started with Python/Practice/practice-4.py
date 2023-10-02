b3729e2a876c0358d2a3b3a6fb43e2371948d36c
Khadeeja <s.khdeej@gmail.com>
2023-10-02 12:00:32 +0400

    feat: add chp1 pract-4

---
+++ Chapter 1- Getting Started with Python/Practice/practice-4.py
@@ -0,0 +1,8 @@
+'''Compute the area of a triangle'''
+
+print("Enter the height: ")
+height = int(input())
+print("Enter the base: ")
+base = int(input())
+area = 1/2 * height * base
+print("Area of triangle: ", area)

121571626eda74b71d3a0a7f5ae40adf6e50cfb0
Khadeeja <s.khdeej@gmail.com>
2023-10-02 11:33:09 +0400

    feat: add chp1 pract-1 & pract-2

---
+++ Chapter 1- Getting Started with Python/Practice/practice-1.py
@@ -0,0 +1,3 @@
+'''Write a program to output the letters WXYZ, each on a separate line'''
+
+print("W\nX\nY\nZ")
---
+++ Chapter 1- Getting Started with Python/Practice/practice-2.py
@@ -0,0 +1,4 @@
+'''Create a program to output "hi" 50 times without any separator'''
+
+greeting = "hi"
+print(50*greeting)

373219d2cb07581be379403632afd30a6a6569b4
Khadeeja <s.khdeej@gmail.com>
2023-10-12 11:18:40 +0400

    feat: chp1 act-1

---
+++ Chapter 1- Getting Started with Python/Practice/activity-1.py
@@ -0,0 +1,13 @@
+#create a personal profile and take inputs
+
+name = input("\nEnter your name: ")
+age = int(input("Enter your age: "))
+student = input("Are you a student? If yes, where do you study? \n")
+work = input("Do you work? If yes, what is your job? \n")
+balance = float(input("Enter you bank balance: "))
+siblings = input("How many siblings do you have? \n")
+hobby = input("What are you passionate about? \n")
+movie = input("What is your fav movie? \n")
+actor = input("Who is your fav actor? \n")
+stress = input("How do you handle stress? \n")
+motivation = input("What motivates you? \n"))

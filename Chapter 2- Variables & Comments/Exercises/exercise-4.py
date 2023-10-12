f859c7a4230c7de925f700cc876982e823aa0f3a
Khadeeja <s.khdeej@gmail.com>
2023-10-12 11:45:38 +0400

    feat: edit chp2 ex-4
    real edit in chp2 ex-4, previous one was placed in an incorrect file

--- Chapter 2- Variables & Comments/Exercises/exercise-4.py
+++ Chapter 2- Variables & Comments/Exercises/exercise-4.py
@@ -3,5 +3,5 @@ Then, using that variable, create a message that reveals your favorite number.
 Print that message.'''
 
 fav_num = 3
-message = "My favourite number is "
-print(message + str(fav_num))
+message = f'My favourite number is {fav_num}.'
+print(message)

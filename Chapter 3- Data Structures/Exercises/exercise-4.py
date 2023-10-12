088afefd6c3939cc36ac94be755a4e7eb199c857
Khadeeja <s.khdeej@gmail.com>
2023-10-12 12:00:49 +0400

    feat: add chp3 ex-4

---
+++ Chapter 3- Data Structures/Exercises/exercise-4.py
@@ -0,0 +1,5 @@
+guests = ["Sappho", "Emily Dickinson", "Anne Lister"]
+message = "I would like to invite you to dinner.\n"
+print(guests[0], message)
+print(guests[1], message)
+print(guests[2], message)

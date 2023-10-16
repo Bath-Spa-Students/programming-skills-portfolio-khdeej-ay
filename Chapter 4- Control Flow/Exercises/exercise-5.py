3fe7d2d146eb41a2cda04ac23643a4449dba8f90
Khadeeja <s.khdeej@gmail.com>
2023-10-16 13:14:07 +0400

    feat: chp4 ex-5 w/ comments

--- Chapter 4- Control Flow/Exercises/exercise-5.py
+++ Chapter 4- Control Flow/Exercises/exercise-5.py
@@ -1,11 +1,17 @@
+#list of three favourite fruits
 favourite_fruits = ["peaches", "green grapes", "clementines"]
+#checks if peaches are in the list
 if "peaches" in favourite_fruits:
     print("You really like peaches!")
+    #checks if mangoes are in the list
 if "mangoes" in favourite_fruits: 
     print("You really like mangoes!")
+#checks if strawberries are in the list
 if "strawberries" in favourite_fruits:
     print("You really like strawberries!")
+#checks if clementines are in the list
 if "clementines" in favourite_fruits:
     print("You really like clementines!")
+#checks if blue berries are in the list
 if "blue berries" in favourite_fruits: 
     print("You really like blue berries!")

426d3c7d7d22a6cc461e4b5d613ab56255f2dd7a
Khadeeja <s.khdeej@gmail.com>
2023-10-16 12:06:39 +0400

    feat: add chp4 ex-2
    first version: runs if statement
    second version: runs else statement

---
+++ Chapter 4- Control Flow/Exercises/exercise-2v1.py
@@ -0,0 +1,12 @@
+#first version (runs the if block)
+
+#variable alien_color contains element green
+alien_color = "green"
+#if statement checks whether the color is green
+if alien_color == "green":
+    #printed if the condition is true
+    print("You have earned 5 points!")
+#else statement runs when the if statement is false (when color is not green)
+else: 
+    #printed if the condition is false (when color is not green)
+    print("You have earned 10 points!")
---
+++ Chapter 4- Control Flow/Exercises/exercise-2v2.py
@@ -0,0 +1,12 @@
+#second version (runs the else block)
+
+#variable alien_color contains element red
+alien_color = "red"
+#if statement checks whether the color is green
+if alien_color == "green":
+    #printed if the condition is true
+    print("You have earned 5 points!")
+#else statement runs when the if statement is false (when color is not green)
+else: 
+    #printed if the condition is false (when color is not green)
+    print("You have earned 10 points!")

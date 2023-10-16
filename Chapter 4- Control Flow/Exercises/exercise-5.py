7893edc971777476460bd3e8106d5646c8b06903
Khadeeja <s.khdeej@gmail.com>
2023-10-16 13:10:58 +0400

    feat: add chp4 ex-5
    independent if statements

---
+++ Chapter 4- Control Flow/Exercises/exercise-5.py
@@ -0,0 +1,11 @@
+favourite_fruits = ["peaches", "green grapes", "clementines"]
+if "peaches" in favourite_fruits:
+    print("You really like peaches!")
+if "mangoes" in favourite_fruits: 
+    print("You really like mangoes!")
+if "strawberries" in favourite_fruits:
+    print("You really like strawberries!")
+if "clementines" in favourite_fruits:
+    print("You really like clementines!")
+if "blue berries" in favourite_fruits: 
+    print("You really like blue berries!")

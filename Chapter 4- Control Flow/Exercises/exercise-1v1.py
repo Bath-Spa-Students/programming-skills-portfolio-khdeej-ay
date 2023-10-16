0c265acdf95ab2e69733f15c517a210b3e108b83
Khadeeja <s.khdeej@gmail.com>
2023-10-16 11:58:14 +0400

    feat: add chp4 ex-1
    both versions do not have else statements because the requirements stated no output should be printed

---
+++ Chapter 4- Control Flow/Exercises/exercise-1v1.py
@@ -0,0 +1,8 @@
+#first version of chp4 exercise 1
+
+#variable alien_color is green
+alien_color = "green"
+#if statement to check whether alien_color is green
+if alien_color == "green":
+    #printing that the player has earned 5 points if the condition is true
+    print("Yay! You have earned 5 points.")
---
+++ Chapter 4- Control Flow/Exercises/exercise-1v2.py
@@ -0,0 +1,8 @@
+#second version of chp4 exercise 1 (produces no output)
+
+#variable alien_color is red
+alien_color = "red"
+#if statement to check whether alien_color is green
+if alien_color == "green":
+    #printing that the player has earned 5 points if the condition is true
+    print("Yay! You have earned 5 points.")

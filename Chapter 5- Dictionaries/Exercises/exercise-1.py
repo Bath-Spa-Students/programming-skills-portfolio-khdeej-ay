3516d186300c81e902bd4d1f773becbd9709842c
Khadeeja <s.khdeej@gmail.com>
2023-10-23 11:39:25 +0400

    feat: add chp5 ex-1

---
+++ Chapter 5- Dictionaries/Exercises/exercise-1.py
@@ -0,0 +1,15 @@
+# a dictionary storing my friend's details.
+friend = {
+    "first_name": "Amna",  #her first name
+    "last_name": "Arif",   #her last name
+    "age": 19,             #her age
+    "city": "Lahore"       #city she lives in
+}
+# printing friend's first name
+print(friend["first_name"])
+# printing friend's last name
+print(friend["last_name"])
+# printing friend's age
+print(friend["age"])
+#printing friend's city
+print(friend["city"])

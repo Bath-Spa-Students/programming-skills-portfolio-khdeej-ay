c79992c33fe16430b65da62dd2f3b84162c49f3f
Khadeeja <s.khdeej@gmail.com>
2023-10-30 12:28:16 +0400

    feat: add chp6 ex-3

---
+++ Chapter 6- Loops/Exercises/exercise-3.py
@@ -0,0 +1,7 @@
+'''Write a loop that never ends, and run it. 
+(To end the loop, press ctrl-C or close the window displaying the output.)'''
+
+# never-ending / infinite loop 
+while True: 
+    # prints 1 forever because there is no break statement
+    print(1)

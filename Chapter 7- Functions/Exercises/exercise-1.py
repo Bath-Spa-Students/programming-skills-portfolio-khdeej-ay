75f625235c63220bdfc40c01bc758b4188ba4f55
Khadeeja <s.khdeej@gmail.com>
2023-11-02 11:28:51 +0400

    feat: add chp7 ex-1

---
+++ Chapter 7- Functions/Exercises/exercise-1.py
@@ -0,0 +1,11 @@
+'''Write a function called display_message() that prints one sentence 
+telling everyone what you are learning about in this chapter. Call the
+function, and make sure the message displays correctly.'''
+
+# def is used to define the display_message function
+def display_message():
+    # prints a message about functions
+    print("A function is a block of code which only runs when it is called.")
+  
+# call the display_message function
+display_message()

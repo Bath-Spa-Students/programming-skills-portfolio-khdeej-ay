2f9b84d1a41be6b8f0cab26761ff385893b61fd1
Khadeeja <s.khdeej@gmail.com>
2023-11-15 07:18:11 +0400

    feat: chp3 full

--- Chapter 3- Data Structures/Exercises/exercise-6.py
+++ Chapter 3- Data Structures/Exercises/exercise-6.py
@@ -1,57 +1,69 @@
-#create a list of guests I would like to invite
+'''You just found out that your new dinner table won't arrive in time 
+for the dinner, and you have space for only two guests.
+• Print a message saying that you can invite only two people for dinner.
+• Use pop() to remove guests until only two names remain. Each time you 
+pop a name, print a message to that person letting them know you're sorry. 
+• Print a message to each of the two people still on your list, letting 
+them know they're still invited.
+• Use del to remove the last two names, so you have an empty list. Print 
+your list to make sure it is actually empty at the end of your program.'''
+
+# list of guests I would like to invite
 guests = ["Maha", "Sappho", "Emily", "Anne"]
 
-#variable to store the message of the invitation
+# variable to store the message of the invitation
 message = "I would like to invite you to dinner."
 
-#print the invitation for each friend
+# print the invitation for each friend
 print(f"{guests[0]}, {message}")     #guest 1 at index 0
 print(f"{guests[1]}, {message}")     #guest 2 at index 1
 print(f"{guests[2]}, {message}")     #guest 3 at index 2
 print(f"{guests[3]}, {message}")     #guest 4 at index 3
 
-#store the message of the person who cannot make it to dinner
-absent = "I am sorry you can't make it to dinner. Hope you are doing alright!"
+# store the message to the person who cannot make it to dinner
+absent = "I am sorry you can't make it to dinner. Hope you are alright!"
 
-#print the message to the person who cannot make it at index 1
+# print a message to the person who cannot make it
 print(f"\n{guests[1]}, {absent}")
 
 '''modify the list at the index of the person who couldn't make it 
 with the new person I am inviting'''
 guests[1] = "Amna"
 
-#print the second group of invitation messages one for each guest in the list
+# print second group of invitation messages for each guest in the list
 print(f"\n{guests[0]}, {message}")     #guest 1 at index 0
 print(f"{guests[1]}, {message}")     #guest 2 at index 1
 print(f"{guests[2]}, {message}")     #guest 3 at index 2
 print(f"{guests[3]}, {message}")     #guest 4 at index 3
 
-#message that I can only invite two people to dinner
-limit = "I have just been informed that I can only invite two people to dinner"
-#print this message informing them about the guest limit being 2 people
+# message that I can only invite two people to dinner
+limit = "I have just been informed I can only invite two people to dinner"
+# print this message informing them about the guest limit being 2
 print(f"\nTo all the guests I had invited to dinner, {limit}.")
 
-#removing the guest at index 3: Anne
+# remove the guest at index 3: Anne
 uninvite_1 = guests.pop(3)
-#print an apology to Anne, the guest at index 2
-print(f"\n{uninvite_1}, I am unable to keep my word of inviting you to dinner. I apologize for any inconvenience I may have caused.")
+# print an apology to Anne, the guest at index 2
+print(f"""\n{uninvite_1}, I am unable to keep my word of inviting you to 
+      dinner. I apologize for any inconvenience I may have caused.""")
 
-#removing the guest at index 2: Emily
+# remove the guest at index 2: Emily
 uninvite_2 = guests.pop(2)
-#print an apology to Emily, the guest at index 3
-print(f"{uninvite_2}, I am unable to keep my word of inviting you to dinner. I apologize for any inconvenience I may have caused.")
+# print an apology to Emily, the guest at index 3
+print(f"""{uninvite_2}, I am unable to keep my word of inviting you to 
+      dinner. I apologize for any inconvenience I may have caused.""")
 
-#message to both guests that they are on the list and still invited
+# message to final guests that they are on the list and still invited
 invited = "You are still invited to dinner. I hope to see you there."
 
-#print invitation to guest at index 0
+# print invitation to guest at index 0
 print(f"\nCongratulations {guests[0]}! {invited}")
 
-#print invitation to guest at index 1
+# print invitation to guest at index 1
 print(f"Congratulations {guests[1]}! {invited}")
 
-#using del to delete the entire list 
-del guests[: ]
+# using del to delete the entire list 
+del guests[:]
 
-#print list to make sure it is empty
+# print list again to make sure it is empty
 print(f"\nEmpty guest list: {guests}")
--- Chapter 3- Data Structures/Exercises/exercise-7.py
+++ Chapter 3- Data Structures/Exercises/exercise-7.py
@@ -1,41 +1,60 @@
-#list of places I would like to visit (not in alphabetical order)
+'''Think of at least five places in the world you'd like to visit.
+• Store the locations in a list. Make sure it is not in alphabetical order.
+• Print your list in its original order. Don't worry about printing it neatly,
+just print it as a raw Python list.
+• Use sorted() to print it in alphabetical order without modifying the 
+actual list.
+• Show that your list is still in its original order by printing it.
+• Use sorted() to print your list in reverse alphabetical order without 
+changing the order of the original list.
+• Show that your list is still in its original order by printing it again.
+• Use reverse() to change the order of your list. Print the list to show 
+that its order has changed.
+• Use reverse() to change the order of your list again. Print the list to 
+show it's back to its original order.
+• Use sort() to change your list so it's stored in alphabetical order. 
+Print the list to show that its order has been changed.
+• Use sort() to change your list so it's stored in reverse alphabetical order. 
+Print the list to show that its order has changed.'''
+
+# list of places I would like to visit (not in alphabetical order)
 places = ["London", "Rome", "NYC", "Berlin", "Greece", "Georgia"]
 
-#print list in original order
+# print list in original order
 print(places)
 
-#sorting list in alphabetical order without changing the actual list
+# sort list in alphabetical order without changing the actual list
 sorted_places = sorted(places)
-#printing the alphabetical list
+#print the alphabetical list
 print(sorted_places)
 
-#proving that the actual list is in the original order
+# prove that the actual list is in the original order
 print(places)
 
-#sorting list in reverse order without changing the actual list
+# sort list in reverse order without changing the actual list
 reversed_places = sorted(places, reverse=True)
-#printing the reverse list
+# print the reverse list
 print(reversed_places)
 
-#proving AGAIN that the actual list is in the original order
+# prove AGAIN that the actual list is in the original order
 print(places)
 
-#using the reverse function to reverse the list elements
+# use the reverse function to reverse the list elements
 places.reverse()
-#printed proof that the actual list is now changed to the reversed list
+# print proof that the actual list is now changed to the reversed list
 print(places)
 
-#using reverse AGAIN to revert the list back to its original order
+# use reverse AGAIN to revert the list back to its original order
 places.reverse()
-#printing the list after reversing it twice; back to the original list
+# print the list after reversing it twice; back to the original list
 print(places)
 
-#using the sort function to sort the actual list in alphabetical order
+# use the sort function to sort the actual list in alphabetical order
 places.sort()
-#printed proof that the actual list is now changed to the alphabetical list
+# print proof that the actual list is now changed to the alphabetical list
 print(places)
 
-#using sort AGAIN to revert the list back to its original order
+# use sort AGAIN to revert the list back to its original order
 places.sort()
-#printing the list after sorting it twice; back to the original list
+# print the list after sorting it twice; back to the original list
 print(places)
